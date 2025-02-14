d5ffbdc64b95a7e2f56fd6f5c3f27aa2101d5cb0
luan-dev <progluan@gmail.com>
2025-02-13 20:50:29 -0500

    RELEASE 1.0.0

--- cmd/start.go
+++ cmd/start.go
@@ -14,8 +14,8 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	discordAppID := os.Getenv("DISCORD_APP_ID")
-	discordBotToken := os.Getenv("DISCORD_BOT_TOKEN")
+	discordAppID := os.Getenv("HACKCLAW_DISCORD_APP_ID")
+	discordBotToken := os.Getenv("HACKCLAW_DISCORD_BOT_TOKEN")
 
 	bot.DiscordAppID = discordAppID
 	bot.DiscordBotToken = discordBotToken
